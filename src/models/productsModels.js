d09bad99c7f453bad118d33a2efcc630514f3b2f
Ghofar Asnanto <ghofarasnanto@gmail.com>
2022-06-18 14:51:46 +0700

    update query total page

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -95,6 +95,7 @@ const findProduct = (query) => {
         }
         db.query(sqlQuery, params)
             .then((result) => {
+                const { product_name, category, order, sort, limit = 4, page = 1 } = query;
                 if (result.rows.length === 0) {
                     return reject({ status: 404, err: "product Not Found" });
                 }
