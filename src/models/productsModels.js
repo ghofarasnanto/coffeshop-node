3fc599b547e4c7dc179eb865854b6c7c5cf251df
Ghofar Asnanto <ghofarasnanto@gmail.com>
2022-06-20 15:16:50 +0700

    update sql create product

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -18,13 +18,13 @@ const db = require("../config/db");
 //     });
 // };
 
-const createProduct = (body) => {
+const createProduct = (body, image) => {
     return new Promise((resolve, reject) => {
-        const { product_name, description, price } = body;
+        const { product_name, description, price, delivery_method, product_size, category_id, start_hour, end_hour, stock } = body;
         const sqlQuery =
-            "INSERT INTO products(product_name, description, price, created_at) VALUES ($1, $2, $3, $4);";
+            "INSERT INTO products(product_name, description, price, delivery_method, product_size, category_id, start_hour, end_hour, stock, image, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);";
         const timestamp = new Date(Date.now());
-        db.query(sqlQuery, [product_name, description, price, timestamp])
+        db.query(sqlQuery, [product_name, description, price, delivery_method, product_size, category_id, start_hour, end_hour, stock, image, timestamp])
             .then(result => {
                 const response = {
                     data: result.body,
